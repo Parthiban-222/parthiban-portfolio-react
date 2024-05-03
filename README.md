e484458025edb3053abdb8b5e3d39ca2544c397a
Parthiban-222 <parthibanparthiban6212gmail.com>
2024-05-03 14:16:42 +0530

    Initial commit: Add React project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Portfolio Template:fire:
+Made by using Reactjs.
+
+## Instructions to run:-  
+
+1)Clone the repo and run the following command:
+```npm i```  
+
+2)Edit the resumeData.js file in src folder.
+
+3)To run it on your local machine, open the terminal and run command: 
+```npm start```
+
+4)Generate build using command:  
+ ```npm run build```
+ and deploy it on the server you wish.
+
+Wooahh!!! You have made your portfolio within minutes:smile:
---
+++ src/resumeData.js
@@ -0,0 +1,120 @@
+let resumeData = {
+  "imagebaseurl":"https://devisha.me/React-Portfolio/",
+  "name": "Parathiban S",
+  "role": "Devops Engineer",
+  "linkedinId": "Parthiban s",
+  "mailId": "parthibanparthiban621@gmail.com",
+  "phoneno": "9791459451",
+  "roleDescription": "I like dabbling in various parts of cloud computation and like to learn about new technologies and simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/parthiban-s-b823b4249/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"Parthiban-222",
+        "url":"https://github.com/Parthiban-222",
+        "className":"fa fa-github"
+      },
+      
+      {
+        "name":"twitter",
+        "url":"https://twitter.com/",
+        "className":"fa fa-twitter"
+      },
+      {
+        "name":"instagram",
+        "url":"https://www.instagram.com/",
+        "className":"fa fa-instagram"
+      }  
+      
+    ],
+  "aboutme":"Iam good in java programming and iam currently learning Devops in AWS.",
+  "address":"Cuddalore,Tamil Nadu, India",
+  "mail":"parthibanparthiban621@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Karpagam Institute of Technology",
+      "specialization":"Electrical and Electronics Engineering",
+      "MonthOfPassing":"March",
+      "YearOfPassing":"2025"
+    },
+    //{
+      //"UniversityName":"ABC University",
+      //"specialization":"Some specialization",
+      //"MonthOfPassing":"March",
+      //"YearOfPassing":"2020",
+      //"Achievements":"Some Achievements"
+    //}
+  ],
+  // "work":[
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Oct",
+  //     "YearOfLeaving":"2020",
+  //     "Achievements":"Some Achievements"
+  //   },
+  //   {
+  //     "CompanyName":"Some Company",
+  //     "specialization":"Some specialization",
+  //     "MonthOfLeaving":"Oct",
+  //     "YearOfLeaving":"2020",
+  //     "Achievements":"Some Achievements"
+  //   }
+  // ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"JAVA"
+    },
+    {
+      "skillname":"Cloud Computing"
+    },
+    {
+      "skillname":"PLC"
+    },
+    // {
+    //   "skillname":"MongoDB"
+    // }
+  ],
+  "portfolio":[
+    {
+      "name":"Helmet Based Speed Conroller",
+      "description":"Our project aims is to enhance road safety by implementing our system which controls the speed of a bike based on whether the rider is wearing a helmet or not",
+      "imgurl":"images/portfolio/phone.jpg",
+      "github":"https://github.com/"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg",
+      "github":"https://github.com/"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png",
+      "github":"https://github.com/"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg",
+      "github":"https://github.com/"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
